d9b8e2890f35a73a5ebef9a9577129021def6681
Amapika <haseokid@ymail.com>
2019-09-15 23:49:56 +0700

    change port to process environment port

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 app.get('/', function(req,res){
     res.send("This is working, cap!")
 })
-
-app.listen(80, function(){
+const port = process.env.port || 80 
+app.listen(port, function(){
     console.log('Listening on port 3000!')
 })
 
