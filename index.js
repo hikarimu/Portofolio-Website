0e38ff8fd9b3b4b04d641fa72eba3fb97b381103
Amapika <haseokid@ymail.com>
2019-09-15 23:54:49 +0700

    minor bug, change capitalization

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 app.get('/', function(req,res){
     res.send("This is working, cap!")
 })
-const port = process.env.port || 80 
-app.listen(port, function(){
-    console.log('Listening on port 3000!')
+app.set('port', process.env.PORT || 80 ) 
+app.listen(app.get('port'), function(){
+    console.log('Listening!')
 })
 
